ca370f0161a084da4f292e193353deed58e01fb0
pramodrb2017-cloud <pramodrb2017@gmail.com>
2026-01-29 15:57:05 +0530

    Update main.py
    Added a method call

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def hello();
-print("hi");
-def  bye();
-print("bye")
+def hello():
+    print("hi")
 
+def bye():
+    print("bye")
 
-Print(hello())
+methods = {
+    "1": hello,
+    "2": bye
+}
 
+choice = input("Enter 1 for hello, 2 for bye: ")
+methods[choice]()
